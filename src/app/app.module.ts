20fe0ed8205bdf4e16436c4c79d71eefa3fcb1bf
Johannes Rudolph <jojo.rudolph@gmail.com>
2017-01-08 15:30:10 +0100

    upgrade to angular-cli beta.24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChildComponent,
-    OtherComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
