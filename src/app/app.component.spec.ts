cc1ea75fccc86af7c186e556bda37cbe2de03a12
Johannes Rudolph <jojo.rudolph@gmail.com>
2017-01-09 09:22:51 +0100

    use strict null check and add repro for 'ng test' failing to rais error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,30 +5,13 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-    TestBed.compileComponents();
   });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('violating null check should raise compilation error', () => {
+    let string = '';
+    string = null;
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    const array = [];
+    array.push(1);
+  });
 });
